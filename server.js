cbc2ccc9dbf11f26d46804b740812df369a501da
Raphael Renato Wanderley <rapha.rw@gmail.com>
2017-09-07 07:44:22 -0300

    alert port config openshift

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var app = require('./config/express-config')();
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
- 
-app.listen(port, ip_address, function () {
-  console.log( "Listening on " + ip_address + ", port " + port )
-});
+
+if('127.0.0.1' == ip_address){
+	app.listen(port, function () {
+	  console.log( "Listening on port " + port );
+	});	
+} 
+else{
+	app.listen(port, ip_address, function () {
+	  console.log( "Listening on " + ip_address + ", port " + port )
+	});	
+}
