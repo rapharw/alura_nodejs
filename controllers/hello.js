ad424a3ff42b3cd0f8efa021fe7ec145b04b2bfb
Raphael Renato Wanderley <rapha.rw@gmail.com>
2017-09-07 18:23:51 -0300

    testes

--- controllers/hello.js
+++ controllers/hello.js
@@ -1,6 +1,6 @@
 module.exports = function(app){
     var methodHello = '/hello';
-    app.post(methodGETHello, function(req, resp){
+    app.post(methodHello, function(req, resp){
         console.log('invoking ' + methodHello);
         resp.send({'return':'success'});
         resp.status(200);
