8dab9cb6ee1b1db0e6c6b2a0f8d445502eb1beed
Raphael Renato Wanderley <rapha.rw@gmail.com>
2017-09-07 18:20:10 -0300

    teste webhook

--- controllers/hello.js
+++ controllers/hello.js
@@ -2,7 +2,6 @@ module.exports = function(app){
     var methodHello = '/hello';
     app.post(methodGETHello, function(req, resp){
         console.log('invoking ' + methodHello);
-    
         resp.send({'return':'success'});
         resp.status(200);
     });
