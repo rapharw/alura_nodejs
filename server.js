1176167ec10ee02fb9187a61cffc6862b7f804d3
Raphael Wanderley <rapha.rw@gmail.com>
2018-09-02 19:47:52 -0300

    Update port

--- server.js
+++ server.js
@@ -1,15 +1,6 @@
 var app = require('./config/express-config')();
 
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-
-if('127.0.0.1' == ip_address){
-	app.listen(port, function () {
-	  console.log( "Listening on port " + port );
-	});	
-} 
-else{
-	app.listen(port, ip_address, function () {
-	  console.log( "Listening on " + ip_address + ", port " + port )
-	});	
-}
+var port = process.env.PORT || 5000;
+app.listen(port, function () {
+	console.log( "Listening on port " + port)
+});	
